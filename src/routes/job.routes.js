9852ac71c161da43d36cb33fe0bf14e55814f73a
Absar Mushtaq <absar.mustaq@vortexhive.ai>
2025-04-11 01:27:07 +0500

    fixed route error

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -6,10 +6,8 @@ const authenticate = require('../middlewares/authentication');
 const validate = require('../middlewares/validate');
 const { createJobSchema, updateJobSchema, jobIdSchema } = require('../validators/job.validator');
 
-router.get('/', jobController.getAllJobs);
 router.get('/:id', validate(jobIdSchema, 'params'), jobController.getJobById);
 router.post('/', authenticate, validate(createJobSchema), jobController.createJob);
-router.put('/:id', authenticate, validate(jobIdSchema, 'params'), validate(updateJobSchema), jobController.updateJob);
 router.get('/user/jobs', authenticate, jobController.getUserJobs);
 
 module.exports = router;
