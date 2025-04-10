6b5b50f20c7d50f32816c94d94dd8004a34bb254
Zaid Mughal <muhammad.zaid@vortexhive.ai>
2025-04-10 17:12:39 +0500

    customer api modification

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -4,12 +4,21 @@ const accountController = require('../controllers/account.controller');
 const authenticate = require('../middlewares/authentication');
 const authorized = require('../middlewares/authorized');
 const validate = require('../middlewares/validate');
+const upload = require('../middlewares/multer');
 const { ROLES } = require('../utils/constant');
-const { customerAccountSchema, ustaAccountSchema  } = require('../validators/account.validation');
+const { customerAccountSchema,ustaAccountSchema } = require('../validators/account.validation');
 
 
 // Account Creation/Update Route
-router.post('/customer-creation', authenticate, authorized(ROLES.CUSTOMER), validate(customerAccountSchema), accountController.customerAccount);
+// Handle multipart/form-data with any files
+router.post(
+    '/customer-creation',
+    authenticate,
+    authorized(ROLES.CUSTOMER),
+    upload.single('profilePicture'), // Assuming the field name is 'profilePicture'
+    validate(customerAccountSchema),
+    accountController.customerAccount
+);
 router.post('/usta-creation', authenticate, authorized(ROLES.USTA), validate(ustaAccountSchema), accountController.ustaAccount);
 
 
