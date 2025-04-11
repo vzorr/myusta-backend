de66a11d3bb4c0ec4cc631a114444b49de47e286
Zaid Mughal <muhammad.zaid@vortexhive.ai>
2025-04-11 18:34:04 +0500

    job validate route

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -3,11 +3,13 @@ const express = require('express');
 const router = express.Router();
 const jobController = require('../controllers/job.controller');
 const authenticate = require('../middlewares/authentication');
+const authorized = require('../middlewares/authorized');
 const validate = require('../middlewares/validate');
+const { ROLES } = require('../utils/constant');
 const { createJobSchema, updateJobSchema, jobIdSchema } = require('../validators/job.validator');
 
 router.get('/:id', validate(jobIdSchema, 'params'), jobController.getJobById);
-router.post('/', authenticate, validate(createJobSchema), jobController.createJob);
+router.post('/', authenticate, authorized(ROLES.USTA), validate(createJobSchema), jobController.createJob);
 router.get('/user/jobs', authenticate, jobController.getUserJobs);
 
 module.exports = router;
