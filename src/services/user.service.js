ad4fa0830ee1ef61b74e06655c3d85622b7b33a2
Zaid Mughal <muhammad.zaid@vortexhive.ai>
2025-04-29 21:03:27 +0500

    usta profile details

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,71 @@
-const { User } = require('../models');
+const { User, ProfessionalDetail, Portfolio, Location, Availability } = require('../models');
 const { logger } = require('../utils/logger');
 
 const getUstaProfile = async (userId) => {
   try {
-    const usta = await User.findOne({ where: { id: userId, role: 'usta' } });
+    const usta = await User.findOne({
+      where: { id: userId, role: 'usta' },
+      include: [
+        {
+          model: ProfessionalDetail,
+          as: 'professionalDetail'
+        },
+        {
+          model: Portfolio,
+          as: 'portfolios'
+        },
+        {
+          model: Availability,
+          as: 'availability', 
+          attributes: [ 'id', 'budgetAmount ', 'preferredJobTypes', 'maxDistance' ],
+          include: [
+            {
+              model: Location,
+              as: 'location',
+              attributes: [ 'address', 'description', 'latitude', 'longitude']
+            }
+          ]
+        },
+      ],
+    })
+    if (!usta) {
+      return {
+        success: false,
+        message: 'Usta not found'
+      }
+    }
+
+    return {
+      success: true,
+      message: 'Usta profile fetched successfully',
+      data: {
+        id: usta.id,
+        firstName: usta.firstName,
+        lastName: usta.lastName,
+        phone: usta.phone,
+        profilePicture: usta.profilePicture,
+        professionalDetail: usta.professionalDetail ? {
+          id: usta.professionalDetail.id,
+          nipt: usta.professionalDetail.nipt,
+          experiences: usta.professionalDetail.experiences,
+          portfolios: usta.portfolios ? usta.portfolios.map(item => {
+            return {
+              id: item.id,
+              title: item.title,
+              description: item.description,
+              category: item.category,
+              media: item.media
+            }
+          }) : [],
+        } : {},
+
+        availability: usta.availability ? usta.availability : {},
+
+        
+
+      }
+    };
+
     if (!usta) {
       return { success: false, message: 'Usta not found' };
     }
