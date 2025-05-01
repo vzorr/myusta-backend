2a217048608d090dffd0bc4cef4467a9cff62840
Zaid Mughal <muhammad.zaid@vortexhive.ai>
2025-05-01 20:57:47 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 // Health Check Route
-app.get('/', (req, res) => res.send('OK - Server is running!'));
+app.get('/', (req, res) => res.send('OK - Server is running with pipeline!'));
 
 // Routes
 app.use('/api', routes);
